955b8baf5ce04c8fd5f8f06a90b4da0e0c0218ec
Vaibhav Ingle <vingle@unomaha.edu>
2017-11-11 11:21:31 -0600

    Added application goals

--- frontend/tests/acceptance/list-menus-test.js
+++ frontend/tests/acceptance/list-menus-test.js
@@ -3,10 +3,29 @@ import moduleForAcceptance from 'frontend/tests/helpers/module-for-acceptance';
 
 moduleForAcceptance('Acceptance | list menus');
 
-test('visiting /', function(assert) {
+test('should show specials and offers on the home page', function (assert) {
+});
+
+test('should link to information about the restaurant.', function (assert) {
+});
+
+test('should link to contact information.', function (assert) {
+});
+
+test('should list food items.', function (assert) {
+});
+
+test('should show food items to checkout and social media login.', function (assert) {
+});
+
+test('should show payment details', function (assert) {
+});
+
+
+/* test('visiting /', function(assert) {
   visit('/');
 
   andThen(function() {
     assert.equal(currentURL(), '/');
   });
-});
+}); */
