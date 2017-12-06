d4e78b1481ae9a3b75ca3f13630ff4ce5056d59c
juan <seco007@hotmail.com>
2017-12-05 18:48:06 -0600

    Initializer and services shopping cart

---
+++ frontend/app/services/shopping-cart.js
@@ -0,0 +1,22 @@
+import Service from '@ember/service';
+
+export default Service.extend({
+  items: null,
+
+  init() {
+    this._super(...arguments);
+    this.set('items', []);
+  },
+
+  add(item) {
+    this.get('items').pushObject(item);
+  },
+
+  remove(item) {
+    this.get('items').removeObject(item);
+  },
+
+  empty() {
+    this.get('items').clear();
+  }
+});
